c4efce3e3350bcc0e43856f1313fa2575b41d800
Tomá Zikmund <tommekster@gmail.com>
2018-12-18 08:27:18 +0100

    uživatelské recenze - doplněno o texty a popisky k obrázkům

--- reporty/zikmund/uzivatelskeRecenzeDoGrafu.py
+++ reporty/zikmund/uzivatelskeRecenzeDoGrafu.py
@@ -68,7 +68,7 @@ for t,T in enumerate(['uživatelů', 'kaváren']):
     print('z toho {}'.format(T), [len([n for n in G.nodes if G.nodes[n]['type'] == t]) for G in graphs])
 
 
-# ### Uložení grafu
+# ## Uložení grafu
 
 # In[7]:
 
@@ -77,7 +77,7 @@ for G, param in zip(graphs, params):
     nx.write_gexf(G, 'graph_{}.gexf'.format(param))
 
 
-# ### Vizualizace grafu
+# ## Vizualizace grafu
 # S detekcí komunit: https://github.com/taynaud/python-louvain.
 
 # In[8]:
@@ -90,17 +90,26 @@ def drawGraph(G):
     nx.draw(G, layout, node_size=sizes, node_color=partition)
 
 
+# ### Graf hodnocení prostředí
+# Malé vrcholy jsou uživatelé, velké vrcholy jsou kavárny. Váha hrany představuje hodnocení prostředí kavárny uživatelem.  Barevně jsou rozlišeny skupiny vrcholů, které jsou mezi sebou vzájemně propojeny silněji, než s ostatními. Rozložení vrcholů v obrázku je dáno gravitačním modele: odpudivou silou jader vrcholů a přitažlivou silou hran. 
+
 # In[9]:
 
 
 drawGraph(env_graph)
 
 
+# ### Graf hodnocení chuti kávy
+# Váha hrany v grafu představuje hodnocení chuti kávy z kavárny (větší vrchol) uživatelem (menší vrchol).  Barevně jsou rozlišeny skupiny vrcholů, které jsou mezi sebou vzájemně propojeny silněji, než s ostatními. Rozložení vrcholů v obrázku je dáno gravitačním modele: odpudivou silou jader vrcholů a přitažlivou silou hran. 
+
 # In[10]:
 
 
 drawGraph(taste_graph)
 
+
+# ## Matice sousednosti
+# Řádky a sloupce jsou zpermutovány (`sorted(G.nodes)`) tak, aby matice byla blokové. U bipartitního grafu můžou mít matice blokově nulovou diagonálu. 
 
 # In[11]:
 
@@ -122,6 +131,9 @@ showAdjacency(env_graph)
 showAdjacency(taste_graph)
 
 
+# ## Incidenční matice
+# U bipartitního grafu lze sestavit incidenční matici mezi vrcholy jednoho a druhého typu (tj. zákazníky a kavárny). 
+
 # In[14]:
 
 
@@ -135,39 +147,49 @@ def getIncidency(G):
 # In[15]:
 
 
-def showIncidency(G):
+def showIncidency(G, title=None):
     incidency = getIncidency(G)
     plt.matshow(incidency)
+    plt.xlabel('kavárny')
+    plt.ylabel('zákazníci')
+    if title != None: plt.title(title)
 
 
 # In[16]:
 
 
-showIncidency(env_graph)
+showIncidency(env_graph, 'Hodnocení prostředí')
 
 
 # In[17]:
 
 
-showIncidency(taste_graph)
+showIncidency(taste_graph, 'Hodnocení chuti')
 
 
 # ## Biclustering
+# I. S. Dhillon, S. Mallela, D. S. Modha: Information-Theoretic Co-clustering
+# 
+# Biclustering umožňuje roztřídit kavárny a zákazníky do skupin a nalézt vazby mezi skupinami. 
+# De facto vyrobí pro každou skupinu typického zástupce a určí pro typickouu dvojici určí typickou/průměrnou recenzi. 
+# Tím lze předpovědět některé chybějící recenze od zákazníků. 
+# 
+# Biclustering pustíme na incidenční matici. Z výsledku klastrování lze sestavit aproximaci původní incidenční matice. V podstatě obsahuje i odhady chybějících recenzí. 
 
-# In[23]:
+# In[18]:
 
 
 import sys
-sys.path.append('c:\\Users\\Tomas\\Documents\\FJFI\\1400bp-zikmund-tomas\\Software\\15 TINcc')
+sys.path.append('c:\\Users\\Tomas\\Documents\\GIT\\14009soft\\TINcc')
 import Itcc
 import numpy as np
 
 
-# In[24]:
+# In[19]:
 
 
-initRowClusters = [i % 5 for i,_ in enumerate(customers)]
-initColumnClusters = [i % 5 for i,_ in enumerate(coffees)]
+initRowClusters = [i % 10 for i,_ in enumerate(customers)]
+initColumnClusters = [i % 10 for i,_ in enumerate(coffees)]
 incidencyMatrix = getIncidency(taste_graph)
 model = Itcc.ItccModel(incidencyMatrix/np.sum(incidencyMatrix), initRowClusters, initColumnClusters)
 itcc = Itcc.Itcc()
@@ -175,51 +197,81 @@ itcc.updateClusterMatrices(model)
 itcc.updatePs(model)
 
 
-# In[25]:
+# In[20]:
 
 
-plt.matshow(model.Q)
+plt.matshow(model.Q);
+plt.xlabel('kavárny');
+plt.ylabel('zákazníci');
+plt.title('Přibližně původní incidenční matice');
 
 
-# In[26]:
+# In[21]:
 
 
 itcc.Iteration(model)
 itcc.Evaluation(model)
+
+
+# In[22]:
+
+
+itcc.Iteration(model)
+itcc.Evaluation(model)
+
+
+# In[23]:
+
+
+itcc.Iteration(model)
+itcc.Evaluation(model)
+
+
+# In[24]:
+
+
+itcc.Iteration(model)
+itcc.Evaluation(model)
+
+
+# In[25]:
+
+
+plt.matshow(model.Q);
+plt.xlabel('kavárny');
+plt.ylabel('zákazníci');
+plt.title('Aproximace incidenční matice');
+
+
+# ## Typická hodnocení
+# Výsledkem klastrování jsou skupiny zákazníků reprezentované typickým zákazníkem, skupiny kaváren zastoupené typickou kavárnou a typickým hodnocením typické kavárny typickým zákazníkem. 
+
+# In[26]:
+
+
+plt.matshow(model.PXhatYhat);
+plt.title('Typická hodnocení');
+plt.xlabel('Typická kavárna');
+plt.ylabel('Typický zákazník');
 
 
 # In[27]:
 
 
-itcc.Iteration(model)
-itcc.Evaluation(model)
+print('Počty zákazníků ve skupinách') 
+print('\tSkupina\tPočet')
+for n,v in enumerate(model.CX @ np.ones((model.CX.shape[1],1))): print('\t{}\t{}'.format(n,v))
 
 
 # In[28]:
 
 
-itcc.Iteration(model)
-itcc.Evaluation(model)
-
-
-# In[29]:
-
-
-itcc.Iteration(model)
-itcc.Evaluation(model)
-
-
-# In[30]:
-
-
-plt.matshow(model.Q)
-
-
-# In[31]:
-
-
-plt.matshow(model.PXhatYhat)
+print('Počty kaváren ve skupinách') 
+print('\tSkupina\tPočet')
+for n,v in enumerate(model.CY @ np.ones((model.CY.shape[1],1))): print('\t{}\t{}'.format(n,v))
 
 
 # ## Závěr
-# Algoritmus našel 2 třídy uživatelů a 3 skupiny kaváren. 
+# Algoritmus našel 3 třídy uživatelů a 4 skupiny kaváren. Většina zákazníků a kaváren jsou v jedné skupině a mají pozitivní typickou recenzi. 
+# 
+# Biklastrovací metoda vyžaduje zadat maximální počty klastrů a počáteční rozdělení. Podle toho se odvíjí výsledek. Metodu jsem spustil v úplně základním stavu. 
